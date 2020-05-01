44c18870ef2e3bc43d3bf11a6c7a465a5dd62445
jawadhassan7 <jawadhassan@folio3.com>
2020-05-01 14:55:04 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import * as signalR from "@microsoft/signalr";
 
-function App() {
+const App = () => {
+  // Builds the SignalR connection, mapping it to /chat
+  const hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl("https://167.86.94.72:44385/tickers")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+  console.log({hubConnection});
+  // Starts the SignalR connection
+  hubConnection.start().then((a) => {
+    // Once started, invokes the sendConnectionId in our ChatHub inside our ASP.NET Core application.
+    if (hubConnection.connectionId) {
+      hubConnection.invoke("sendConnectionId", hubConnection.connectionId);
+    }
+  });
+
+  const SignalRTime = () => {
+    // Sets the time from the server
+    const [time, setTime] = useState(null);
+
+    useEffect(() => {
+      hubConnection.on("setTime", (message) => {
+        setTime(message);
+      });
+    });
+
+    return <p>The time is {time}</p>;
+  };
+
+  const SignalRClient = () => {
+    // Sets a client message, sent from the server
+    const [clientMessage, setClientMessage] = useState(null);
+
+    useEffect(() => {
+      hubConnection.on("setClientMessage", (message) => {
+        setClientMessage(message);
+      });
+    });
+
+    return <p>{clientMessage}</p>;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SignalRTime />
+      <SignalRClient />
+    </>
   );
-}
+};
 
 export default App;
